e8df737bc43e1477e3678939e9599724853a3a20
mengxu98 <86040828+mengxu98@users.noreply.github.com>
2023-03-30 09:49:07 +0800

    Update GSE154826.sh

--- GSE154826.sh
+++ GSE154826.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-Folder_A = "/data/mengxu/data/GSE154826" #/GSE154826_amp_batch_ID_1
-cd $Folder_A
+cd ~
+Folder_User="GSE154826" # Please set by yourself
+mkdir $Folder_User
+cd $Folder_User
 wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE154nnn/GSE154826/suppl/GSE154826_sample_annots.csv.gz
 gunzip GSE154826_sample_annots.csv.gz
+
 id=$(cut -d ',' -f 3 GSE154826_sample_annots.csv)
 for i in ${id}; do
     wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE154nnn/GSE154826/suppl/GSE154826_amp_batch_ID_$i.tar.gz
 done
 
-for file_a in ${Folder_A}/*; do
+for file_a in ${Folder_User}/*; do
     temp_folder=$(basename $file_a .tar.gz)
 
     mkdir $temp_folder
